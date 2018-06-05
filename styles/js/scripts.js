0da5d7bbfbd514514886f20b5545dc4cfb54b8e9
NokiNiko <saratlijanikola@gmail.com>
2018-06-05 13:33:08 +0200

    Changes to nav. Removed unnecessary media query.

--- styles/js/scripts.js
+++ styles/js/scripts.js
@@ -1,42 +1,69 @@
-const fadeInElements = document.getElementsByClassName('forFadeIn');
+(function () {
 
-for (let i = 0; i < fadeInElements.length; i++) {
-    new Waypoint({
-        element: fadeInElements[i],
-        handler: function () {
-            this.element.classList.add('fadein')
-        }, offset: '80%'
-    });
-}
+    const fadeInElements = document.getElementsByClassName('forFadeIn');
 
-let navIsShown = false;
-const nav = document.getElementById("nav");
-
-document.getElementById('mobilenav').addEventListener('click', function () {
-    this.classList.toggle('mobilenavblack');
-
-    if (!navIsShown) {
-        TweenLite.set(nav, {'display': 'block', margin: '2vh'});
-        TweenLite.to(nav, .25, {height: '26vh'});
-        navIsShown = true;
-    } else {
-        TweenLite.to(nav, .25, {
-                height: '0',
-                margin: '0',
-                onComplete: function () {
-                    TweenLite.set(nav, {display: 'none'});
-                }
-            }
-        );
-        navIsShown = false;
+    for (let i = 0; i < fadeInElements.length; i++) {
+        new Waypoint({
+            element: fadeInElements[i],
+            handler: function () {
+                this.element.classList.add('fadein')
+            }, offset: '80%'
+        });
     }
-});
+})();
 
-const button = document.getElementById("button");
 
-if (button) {
-    button.addEventListener("click", function (ev) {
-        ev.preventDefault();
-        TweenLite.to(window, 1, {scrollTo: this.getAttribute("href")})
-    })
-}
+(function () {
+
+    let navIsShown = false;
+    const nav = document.getElementById("nav");
+
+    document.getElementById('mobilenav').addEventListener('click', function () {
+        this.classList.toggle('mobilenavblack');
+
+        if (!navIsShown) {
+            TweenLite.set(nav, {'display': 'block', margin: '2vh'});
+            TweenLite.to(nav, .25, {height: '26vh'});
+            navIsShown = true;
+        } else {
+            TweenLite.to(nav, .25, {
+                    height: '0',
+                    margin: '0',
+                    onComplete: function () {
+                        TweenLite.set(nav, {display: 'none'});
+                    }
+                }
+            );
+            navIsShown = false;
+        }
+    });
+})();
+
+
+(function () {
+
+    const button = document.getElementById("button");
+
+    if (button) {
+        button.addEventListener("click", function (ev) {
+            ev.preventDefault();
+            TweenLite.to(window, 1, {scrollTo: this.getAttribute("href")})
+        })
+    }
+})();
+
+
+(function () {
+
+    const anchorsNodeList = document.querySelectorAll("header a");
+    const anchorsArray = Array.from(anchorsNodeList);
+
+    anchorsArray.forEach(function (e) {
+        if (e.getAttribute("href") === "#") {
+            e.classList.add("disabled");
+            e.removeAttribute("href");
+            e.parentElement.classList.remove("li-hover")
+        }
+    });
+})();
+
